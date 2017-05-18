1ac44e48d33700d73a0f29d8592fd47c794afb1a
Royan Jan <royan_jan@126.com>
2017-05-18 20:00:13 +0800

    dubbo consumer & restful controller

---
+++ elune-sample/elune-sample-domain/elune-sample-domain-portal/src/main/java/com/outwit/elune/portal/sample/HelloController.java
@@ -0,0 +1,20 @@
+package com.outwit.elune.portal.sample;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.outwit.elune.service.sample.HelloService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/hello")
+public class HelloController {
+
+    @Reference(version = "1.0.0")
+    private HelloService helloService;
+
+    @RequestMapping(value = "/{user}", method = RequestMethod.GET)
+    public String getHelloMessage(@PathVariable String user) {
+        return helloService.sayHello(user);
+    }
+}
---
+++ elune-sample/elune-sample-domain/elune-sample-domain-portal/src/main/java/com/outwit/elune/portal/sample/RestfulSampleController.java
@@ -0,0 +1,7 @@
+package com.outwit.elune.portal.sample;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/rest")
+public class RestfulSampleController {
+}

f655fd66d18f3eb20adde8d6405b1c992f78a6b0
Royan Jan <royan_jan@126.com>
2017-05-19 19:32:54 +0800

    common module

--- elune-sample/elune-sample-domain/elune-sample-domain-portal/src/main/java/com/outwit/elune/portal/WebApplication.java
+++ elune-sample/elune-sample-domain/elune-sample-domain-portal/src/main/java/com/outwit/elune/portal/WebApplication.java
@@ -2,18 +2,12 @@ package com.outwit.elune.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApplication extends SpringBootServletInitializer {
+public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WebApplication.class);
-    }
 }
--- elune-sample/elune-sample-domain/elune-sample-domain-portal/src/main/java/com/outwit/elune/portal/sample/RestfulSampleController.java
+++
@@ -1,7 +0,0 @@
-package com.outwit.elune.portal.sample;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController(value = "/rest")
-public class RestfulSampleController {
-}
